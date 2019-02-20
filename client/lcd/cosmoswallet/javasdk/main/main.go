2ab35d8232418fd8da095df03d3316f4fc1be9be
Matt Wang <wmd_hust@126.com>
2019-02-20 12:25:39 +0800

    update key import under java sdk

--- client/lcd/cosmoswallet/javasdk/main/main.go
+++ client/lcd/cosmoswallet/javasdk/main/main.go
@@ -12,6 +12,13 @@ func GetAccount(rootDir,node,chainId,addr *C.char) *C.char {
 	return C.CString(output)
 }
 
+//export RecoverKey
+func RecoverKey(rootDir,name,password,seed *C.char) *C.char {
+	output := sdksource.RecoverKey(C.GoString(rootDir),C.GoString(name), C.GoString(password), C.GoString(seed))
+	return C.CString(output)
+}
+
+
 //export TransferAsync
 func TransferAsync(rootDir, node, chainId, fromName, password, toStr, coinStr, feeStr *C.char) *C.char {
 	output := javasdk.TransferAsync(C.GoString(rootDir),C.GoString(node), C.GoString(chainId), C.GoString(fromName),C.GoString(password), C.GoString(toStr), C.GoString(coinStr), C.GoString(feeStr))
