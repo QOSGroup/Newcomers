36565b19d9de62503255b8522981c7949350320c
ms8922 <ms892@163.com>
2019-04-17 19:17:28 +0800

    add AdvertisersTrue

--- client/lcd/cosmoswallet/slim/interface.go
+++ client/lcd/cosmoswallet/slim/interface.go
@@ -726,3 +726,99 @@ func investAd(QOSchainId, QSCchainId, articleHash, coins, privatekey string) (*T
 
 	return tx2, nil
 }
+
+type CoinsTx struct {
+	Address Address
+	Cointype string
+	Amount BigInt
+	ChangeType string     //0 plus  1 minus
+}
+
+//广告商押金或赎回
+func Advertisers( amount, privatekey, cointype,isDeposit,qscchainid string, qscnonce int64) string {
+	var result ResultInvest
+	result.Code = ResultCodeSuccess
+	tx, berr := advertisers(amount, privatekey, cointype,isDeposit,qscchainid, qscnonce)
+	if berr != "" {
+		return berr
+	}
+	js, err := Cdc.MarshalJSON(tx)
+	if err != nil {
+		log.Printf("Advertisers err:%s", err.Error())
+		result.Code = ResultCodeInternalError
+		result.Reason = err.Error()
+		return result.Marshal()
+	}
+	result.Result = json.RawMessage(js)
+	return result.Marshal()
+}
+
+// investAd 投资广告
+func advertisers( coins, privatekey, cointype,isDeposit,qscchainid string,qscnonce int64) (*TxStd, string) {
+	amount, err := strconv.Atoi(coins)
+	if err!=nil {
+		return nil, NewErrorResult("601", 0, "", "amount format error").Marshal()
+	}
+
+	var key ed25519local.PrivKeyEd25519
+	ts := "{\"type\": \"tendermint/PrivKeyEd25519\",\"value\": \"" + privatekey + "\"}"
+	err1 := Cdc.UnmarshalJSON([]byte(ts), &key)
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	priv := key
+	gas := NewBigInt(int64(MaxGas))
+
+	addrben32,_ := bech32local.ConvertAndEncode(PREF_ADD, key.PubKey().Address().Bytes())
+	investor, _ := getAddrFromBech32(addrben32)
+	qscnonce += 1
+	it := &CoinsTx{}
+	it.Address = investor
+	it.Cointype=cointype
+	it.ChangeType=isDeposit
+	it.Amount=NewBigInt(int64(amount))
+	tx:=AdvertisersTx{it}
+	fmt.Println(investor, amount, cointype, isDeposit)
+	tx2 := NewTxStd(tx, qscchainid, gas)
+	signature2, _ := tx2.SignTx(priv, qscnonce, qscchainid)
+	tx2.Signature = []Signature{Signature{
+		Pubkey:    priv.PubKey(),
+		Signature: signature2,
+		Nonce:     qscnonce,
+	}}
+	return tx2, ""
+}
+type AdvertisersTx struct {
+	Tx *CoinsTx
+}
+
+func (tx AdvertisersTx) GetSignData() (ret []byte) {
+	ret = append(ret, tx.Tx.Address.Bytes()...)
+	ret = append(ret, Int2Byte(tx.Tx.Amount.Int64())...)
+	ret = append(ret, []byte(tx.Tx.Cointype)...)
+	ret = append(ret, []byte(tx.Tx.ChangeType)...)
+	return
+}
+
+
+//成为广告商
+//privatekey             //用户私钥
+//coinsType              //押金币种
+//coinAmount             //押金数量
+//qscchainid             //chainid
+//qscnonce               //nonce
+func AdvertisersTrue( privatekey,  coinsType, coinAmount,qscchainid, qscnonce string) string {
+	qsc, _ := strconv.ParseInt(qscnonce, 10, 64)
+	return Advertisers(coinAmount,privatekey,coinsType,"2",qscchainid,qsc)
+}
+
+//成为非广告商 赎回押金
+//privatekey             //用户私钥
+//coinsType              //押金币种
+//coinAmount             //押金数量
+//qscchainid             //chainid
+//qscnonce               //nonce
+func AdvertisersFalse( privatekey,  coinsType, coinAmount,qscchainid, qscnonce string) string {
+	qsc, _ := strconv.ParseInt(qscnonce, 10, 64)
+	return Advertisers(coinAmount,privatekey,coinsType,"1",qscchainid,qsc)
+}
